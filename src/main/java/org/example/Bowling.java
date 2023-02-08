a94609c6be8f6288c025f3929a23f0fd681a6830
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-02-08 21:59:36 +0000

    Initial Commit

---
+++ src/main/java/org/example/Bowling.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Bowling {
+
+    private static final String STRIKE = "X";
+    private static final String SPARE = "/";
+    private static final String NOSCORE = "-";
+
+    public int calculateScore(String scoreSheet) {
+        return 301;
+    }
+}
---
+++ src/test/java/org/example/BowlingTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BowlingTest {
+    @Test
+    void BowlingScoreMax() {
+    }
+
+    @Test
+    void BowlingScoreAllSpares() {
+    }
+}
