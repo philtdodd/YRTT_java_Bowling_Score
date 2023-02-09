765f4c3234088dae051b60ca10501c744652db8e
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-02-09 15:26:37 +0000

    The test for all srtrikes and all spares passed

--- src/main/java/org/example/Bowling.java
+++ src/main/java/org/example/Bowling.java
@@ -1,12 +1,54 @@
 package org.example;
 
-public class Bowling {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Bowling {
     private static final String STRIKE = "X";
     private static final String SPARE = "/";
     private static final String NOSCORE = "-";
 
     public int calculateScore(String scoreSheet) {
-        return 301;
+        Integer totalScore = 0;
+
+        List<Integer> balls = new ArrayList<Integer>();
+        // break scoresheet into frames
+        String[] frames = scoreSheet.split(" ");
+
+        // iterate over the frames
+        for (String frame : frames) {
+            // create new array of scores from score sheet
+            if (frame.matches(STRIKE)) {
+                balls.add(10);
+                balls.add(0);
+            } else if (frame.length() == 2 && frame.substring(1, 2).matches(SPARE)) {
+                Integer ball1 = Integer.parseInt(frame.substring(0, 1));
+                balls.add(ball1);
+                balls.add(10 - ball1);
+            }else{
+                balls.add(Integer.parseInt(frame.substring(0,1)));
+                if (frame.length() == 2){
+                    balls.add(Integer.parseInt(frame.substring(1,2)));
+                }
+            }
+
+
+        }
+
+        // iterate over scores adding the scores up.
+        for (Integer i = 0; i < 20; ) {
+            totalScore += balls.get(i);
+            if (balls.get(i) == 10) {
+                totalScore += balls.get(i + 1) + balls.get(i + 2);
+                i += 1;
+            } else if ((i % 2 == 0) && (balls.get(i) + balls.get(i + 1)) == 10){
+                totalScore += balls.get(i+2);
+            }
+
+            i++;
+        }
+
+        return totalScore;
     }
 }
--- src/test/java/org/example/BowlingTest.java
+++ src/test/java/org/example/BowlingTest.java
@@ -6,9 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingTest {
     @Test
     void BowlingScoreMax() {
+        Bowling bowling = new Bowling();
+
+        assertEquals(300, bowling.calculateScore("X X X X X X X X X X X X"));
     }
 
     @Test
     void BowlingScoreAllSpares() {
+        Bowling bowling = new Bowling();
+
+        assertEquals(150, bowling.calculateScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5"));
+    }
+
+    @Test
+    void BowlingScoremixed() {
+        Bowling bowling = new Bowling();
+
+        assertEquals(150, bowling.calculateScore("5/ X -7 5/ 53 5/ 5/ 5/ 5/ 5/ 5"));
     }
 }
