733bd273d7daff4c42e3f879f45ffe52d358c388
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-02-10 00:30:24 +0000

    Added Passed csv tests

--- src/main/java/org/example/Bowling.java
+++ src/main/java/org/example/Bowling.java
@@ -35,15 +35,17 @@ public class Bowling {
                 balls.add(Integer.parseInt(frames.substring(i, i + 1)));
             }
         }
-
+        if (scoreSheet.contains("54 -/ 61 36 9/ 9/ 23 3- 5/ 7/ 2")) {
+            int a = 0;
+        }
         // Work out if the score card is complete or not.
         Integer max = 20;
         if (balls.size() < 20)
             max = balls.size();
 
         // Check frames add up to 10 or less.
-        for (Integer i = 0; (i < max-1); i += 2)
-            if (i != max && balls.get(i) + balls.get(i+1) > 10)
+        for (Integer i = 0; (i < max - 1); i += 2)
+            if (i != max && balls.get(i) + balls.get(i + 1) > 10)
                 throw new InvalidParameterException("Invalid Score Sheet");
 
         // Calculate the score
@@ -51,19 +53,19 @@ public class Bowling {
             // add this ball to the score
             totalScore += balls.get(i);
             // handle looking forward if it is a strike or a spare
-            if (balls.get(i) == 10) { // STRIKE
+            if ((i % 2) == 0 && balls.get(i) == 10) { // STRIKE
                 // add next frame ball 1 if in array
                 if ((i + 2) < balls.size()) {
                     totalScore += balls.get(i + 2);
                 }
 
                 // if next frame ball 1 not strike add ball two from next frame.
-                if ((i + 3) < balls.size() && balls.get(i+3) != 0) {
+                if ((i + 3) < balls.size() && balls.get(i + 3) != 0) {
                     totalScore += balls.get(i + 3);
                 }
 
                 // if first ball of next frame a strike then add the next frames first ball.
-                if ((i + 4) < balls.size() && balls.get(i+2) == 10) {
+                if ((i + 4) < balls.size() && balls.get(i + 2) == 10) {
                     totalScore += balls.get(i + 4);
                 }
 
